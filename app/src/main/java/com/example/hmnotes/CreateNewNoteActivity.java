80ecf49e5efc990c55e74e5fc4237416d08088b7
enteilegend <hyt2@illinois.edu>
2019-12-07 15:39:19 -0600

    Working on noteObject

--- app/src/main/java/com/example/hmnotes/CreateNewNoteActivity.java
+++ app/src/main/java/com/example/hmnotes/CreateNewNoteActivity.java
@@ -3,9 +3,12 @@ package com.example.hmnotes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.EditText;
 import androidx.appcompat.widget.*;
 
+//Creates a new Note to display
 public class CreateNewNoteActivity extends AppCompatActivity {
 
     Toolbar topBar;
@@ -22,4 +25,30 @@ public class CreateNewNoteActivity extends AppCompatActivity {
         noteBody = findViewById(R.id.noteBody);
         noteSubject = findViewById(R.id.noteSubject);
     }
+
+    //detecting the change in texts
+    public void changingSubjectChangeListener() {
+        noteSubject.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int numOne,
+                                          int numTwo, int numThree) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int numOne,
+                                      int numTwo, int numThree) {
+
+                if (charSequence != null && charSequence.length() > 0) {
+                    getSupportActionBar().setTitle(charSequence);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+    }
+
 }
